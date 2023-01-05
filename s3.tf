e95ab18d74f25ce6ba461b9f1f3bac30c630e061
imprintnext-rajat <121029291+imprintnext-rajat@users.noreply.github.com>
2023-01-05 12:22:00 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -6,8 +6,8 @@ resource "aws_s3_bucket" "b" {
     allowed_headers = ["*"]
     allowed_methods = ["GET","HEAD"]
     allowed_origins = ["*"]
-    expose_headers  = ["ETag"]
-    max_age_seconds = 3000
+    expose_headers  = []
+    // max_age_seconds = 3000
   }
 }
 
