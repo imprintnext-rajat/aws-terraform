4b755cfce53614e5f04bb108d8bbfe40798b611c
imprintnext-rajat <121029291+imprintnext-rajat@users.noreply.github.com>
2023-01-05 12:15:54 +0530

    Create s3.tf

---
+++ s3.tf
@@ -0,0 +1,51 @@
+data "aws_canonical_user_id" "current" {}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "ruswqr"
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET","HEAD"]
+    allowed_origins = ["*"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
+}
+
+resource "aws_s3_bucket_acl" "e" {
+  bucket = aws_s3_bucket.b.id
+  access_control_policy {
+    grant {
+      grantee {
+        id   = data.aws_canonical_user_id.current.id
+        type = "CanonicalUser"
+      }
+      permission = "FULL_CONTROL"
+    }
+
+    grant {
+      grantee {
+        type = "Group"
+        uri  = "http://acs.amazonaws.com/groups/s3/LogDelivery"
+      }
+      permission = "FULL_CONTROL"
+    }
+    grant {
+      grantee {
+        type = "Group"
+        uri  = "http://acs.amazonaws.com/groups/global/AllUsers"
+      }
+      permission = "FULL_CONTROL"
+    }
+     grant {
+      grantee {
+        type = "Group"
+        uri  = "http://acs.amazonaws.com/groups/global/AuthenticatedUsers"
+      }
+      permission = "FULL_CONTROL"
+    }
+
+    owner {
+      id = data.aws_canonical_user_id.current.id
+    }
+  }
+}
